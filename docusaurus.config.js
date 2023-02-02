c2201f04a3b4a5bb8deed5d7bebe43961e0bd06d
Zoyn <602723113@qq.com>
2023-02-02 20:24:16 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'asahi/intro',
+            docId: 'asahi/asahi',
             position: 'left',
             label: 'Asahi',
           },
