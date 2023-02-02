e46705cdfaedfa1e2752bd5e126c7edeef40c8b4
Zoyn <32706754+602723113@users.noreply.github.com>
2023-02-02 19:58:47 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Asahi - Wiki',
+  tagline: 'Everything comes from Asahi.',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: '',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -67,11 +67,18 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Asahi',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'AttributeSystem',
+            to: '/attribute',
+          },
+          {to: '/blog', label: '灵感', position: 'left'},
+          {
+            href: 'https://github.com/602723113/Asahi-Wiki',
             label: 'GitHub',
             position: 'right',
           },
