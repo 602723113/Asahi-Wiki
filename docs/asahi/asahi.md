2957d60b68f1ab04d74945619f5bc614d278ace9
Zoyn <602723113@qq.com>
2023-02-02 20:21:57 +0800

    update

---
+++ docs/asahi/asahi.md
@@ -0,0 +1,6 @@
+---
+sidebar_position: 1
+title: Asahi
+id: asahi
+---
+Asahi 是由 Glom-c 所开发的基于 Taboolib 的新时代 Bukkit 脚本语言
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'asahi/intro',
             position: 'left',
             label: 'Asahi',
           },
